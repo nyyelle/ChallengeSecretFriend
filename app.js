0ca55888478193f313e9de9b3dabbe4fd8d10518
Janyelle <janyelleoliveira50@gmail.com>
2025-01-30 11:30:38 -0300

    Adicionar arquivos do projeto

---
+++ app.js
@@ -0,0 +1,9 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Adicionar nomes
+
+// Validar entrada
+
+// Visualizar a lista
+
+// Sorteio aleatório
